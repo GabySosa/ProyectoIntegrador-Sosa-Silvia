dc1792980f00bbcc57937d90a6f2a08147704b48
MatiaSalvatore <gsosa16.sgs@gmail.com>
2023-05-14 19:36:18 -0300

    Alotofchanges

--- ProyectoIntegrador_Sosa_Silvia/BackEnd/src/main/java/com/portfolio/sosasilvia/Controller/PersonaController.java
+++ ProyectoIntegrador_Sosa_Silvia/BackEnd/src/main/java/com/portfolio/sosasilvia/Controller/PersonaController.java
@@ -5,6 +5,8 @@ import com.portfolio.sosasilvia.Entity.Persona;
 import com.portfolio.sosasilvia.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,16 +16,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin (origins = "http://localhost:4200")
 @RestController
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
-    @GetMapping("/personas/traer")
     
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
@@ -45,4 +48,10 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         return persona;
     }   
+    @GetMapping("/personas/traer/perfil")
+        public Persona findPersona(){
+        return ipersonaService.findPersona((long)5);
+    }
+    
+    
 }
--- ProyectoIntegrador_Sosa_Silvia/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador_Sosa_Silvia/FrontEnd/src/app/app.module.ts
@@ -14,7 +14,11 @@ import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
-
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 
 @NgModule({
   declarations: [
@@ -28,14 +32,20 @@ import {HttpClient, HttpClientModule} from '@angular/common/http';
     HysComponent,
     ProyectoComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    NewExperienciaComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
